3f2dddad0970b492962137ddff6f7b42b7042b9f
Andineitor <andiloor2809@gmail.com>
2023-12-28 16:51:26 -0500

    reedireccionamiento v3.5

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink
 
 // Cambia la ruta para manejar el restablecimiento de contraseña a una solicitud POST
 // Agrega esta línea en tu archivo de rutas o en el middleware global
-Route::middleware('cors')->post('/reset-password/{token}', function ($token) {
+Route::middleware('cors')->get('/reset-password/{token}', function ($token) {
     $resetUrl = "https://cargod.netlify.app/reset-password/$token";
     return Redirect::away($resetUrl);
 })->name('password.reset');
