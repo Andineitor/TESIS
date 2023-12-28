5395ac462c36592ac8656b81b1c72ed0d65705ed
Andineitor <andiloor2809@gmail.com>
2023-12-28 18:15:04 -0500

    reedireccionamiento 5

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/registro', [AuthController::class, 'registro']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
