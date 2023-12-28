5655ffd56fa1c436e1bf1425255a484c5adcdcf5
Andineitor <andiloor2809@gmail.com>
2023-12-28 11:37:57 -0500

    reedireccionamiento v3.1

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
+
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Support\Facades\Password;
@@ -35,7 +36,7 @@ class ForgotPasswordController extends Controller
      * Send a reset link to the given user.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function sendResetLinkEmail(Request $request)
     {
@@ -45,18 +46,24 @@ class ForgotPasswordController extends Controller
             $request->only('email')
         );
 
-        if ($status === Password::RESET_LINK_SENT) {
-            return response()->json(['status' => __('Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.')]);
-        } else {
-            return response()->json(['email' => __('La dirección de correo electrónico proporcionada no se encontró en nuestros registros.')], 400);
-        }
-    }
+        return $status === Password::RESET_LINK_SENT
+        ? response()->json(['status' => __('Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.')])
+        : response()->json(['email' => __('La dirección de correo electrónico proporcionada no se encontró en nuestros registros.')], 400);
+}
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * @param  string  $token
+     * @return \Illuminate\View\View
+     */
+    
 
     /**
      * Reset the given user's password.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function reset(Request $request)
     {
@@ -76,8 +83,12 @@ class ForgotPasswordController extends Controller
             }
         );
 
-        if ($status === Password::PASSWORD_RESET) {
+        // Verifica el $status y devuelve la URL de redirección según corresponda
+        if ($status == Password::PASSWORD_RESET) {
+            // Obtén el token desde la solicitud
             $token = $request->input('token');
+        
+            // Puedes personalizar la URL de redirección aquí y agregar el token
             $redirectUrl = "https://cargod.netlify.app/reset-password/{$token}";
 
             return response()->json(['status' => __($status), 'redirect_url' => $redirectUrl]);
