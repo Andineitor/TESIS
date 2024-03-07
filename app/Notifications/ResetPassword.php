67938612af389d56021852fcca140ba66aef7c9a
Andineitor <andiloor2809@gmail.com>
2024-03-06 22:52:41 -0500

    implementacion de logo y correccion de readme

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+    public $token;
+
+    
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable, $url): MailMessage
+    {
+        return (new MailMessage)
+            ->subject(Lang::get('Notificación de restablecimiento de contraseña'))
+            ->line(Lang::get('Estás recibiendo este correo electrónico porque hemos recibido una solicitud de restablecimiento de contraseña para tu cuenta.'))
+            ->action(Lang::get('Restablecer contraseña'), $url)
+            ->line(Lang::get('Este enlace de restablecimiento de contraseña caducará en :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Si no solicitaste un restablecimiento de contraseña, no es necesario que realices ninguna otra acción.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
